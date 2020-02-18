276152b54089a49a8c91c0e2dc79a7c0b749c1d4
git-leon <xleonhunter@gmail.com>
2020-02-18 09:52:51 -0500

    added the correct driver to the JDBCConfigurator

--- src/main/java/com/github/perscholas/JdbcConfigurator.java
+++ src/main/java/com/github/perscholas/JdbcConfigurator.java
@@ -1,5 +1,9 @@
 package com.github.perscholas;
 
+import com.mysql.cj.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConfigurator {
     public static void initialize() {
     }
