514e08f55d667e26e1c875bf36229a901cd403b1
git-leon <xleonhunter@gmail.com>
2020-02-18 13:35:45 -0500

    updated DatabaseConnection

--- src/main/java/com/github/perscholas/DatabaseConnection.java
+++ src/main/java/com/github/perscholas/DatabaseConnection.java
@@ -9,45 +9,25 @@ public enum DatabaseConnection {
     MYSQL;
 
     public Connection getConnection() {
-        return getConnection(name().toLowerCase());
-    }
-
-    public void executeStatement(String sqlStatement) {
-        try {
-            Statement statement = getScrollableStatement();
-            statement.execute(sqlStatement);
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
-    }
-
-    public ResultSet executeQuery(String sqlQuery) {
-        try {
-            Statement statement = getScrollableStatement();
-            return statement.executeQuery(sqlQuery);
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
-    }
-
-    private Statement getScrollableStatement() {
-        int resultSetType = ResultSet.TYPE_SCROLL_INSENSITIVE;
-        int resultSetConcurrency = ResultSet.CONCUR_READ_ONLY;
-        try {
-            return getConnection().createStatement(resultSetType, resultSetConcurrency);
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
-    }
-
-    private Connection getConnection(String dbVendor) {
         String username = "root";
         String password = "";
+        String dbVendor = name().toLowerCase();
         String url = "jdbc:" + dbVendor + "://127.0.0.1/";
         try {
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             throw new Error(e);
         }
+    }
+
+    public void executeStatement(String sqlStatement) {
+    }
+
+    public ResultSet executeQuery(String sqlQuery) {
+        return null;
+    }
+
+    private Statement getScrollableStatement() {
+        return null;
     }
 }
