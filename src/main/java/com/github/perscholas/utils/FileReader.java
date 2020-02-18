5620bfd28feeeee1ac15d630c522e485a3c9b63f
git-leon <xleonhunter@gmail.com>
2020-02-18 12:03:36 -0500

    eliminated nullpointer

--- src/main/java/com/github/perscholas/utils/FileReader.java
+++ src/main/java/com/github/perscholas/utils/FileReader.java
@@ -2,6 +2,7 @@ package com.github.perscholas.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
@@ -19,19 +20,12 @@ public class FileReader {
 
     @Override
     public String toString() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(filename).getFile());
-        StringBuilder result = new StringBuilder();
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                result.append(line).append("\n");
-            }
+        try {
+            byte[] readAllBytes = java.nio.file.Files.readAllBytes(Paths.get( filename ));
+            return new String( readAllBytes );
         } catch (IOException e) {
             throw new Error(e);
         }
-        return result.toString();
     }
-
 }
 
